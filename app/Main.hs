d4ba29a21b331dc4ac272553f8ad56b1500f3510
Isaac Hiram Lopez Diaz <isaac.lopez@upr.com>
2024-10-14 13:10:19 -0400

    making parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Control.Monad (void, when)
+import System.Environment (getArgs)
+import System.IO (hFlush, stdout)
+
+quit :: String -> IO ()
+quit c = when c == "quit" || c == "q" pure ()
+
+repl :: IO String
+repl = do
+    putStr "rc> "
+    hFlush stdout
+    c <- getLine
+    quit c
+    repl
+
+eval :: IO ()
+eval = undefined
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    case args of
+        [] -> repl
+        [f] -> eval f
+        _ -> putStrLn "Usage: rc [FILE]"
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,26 +1,62 @@
-{-# LANGUAGE TypeOperators #-}
-
 module Parser where
 
-import Expr
+import Control.Monad (void)
 import Text.ParserCombinators.Parsec
 
-parseBool :: Parser (Expr Bool)
-parseBool = (string "true" >> pure (LitBool True)) <|> (string "false" >> pure (LitBool False))
+data Expr a
+    = LitBool Bool
+    | LitInt Int
+    | LitIden String
+    | DefIden String String
+    | UnaOp String (Expr a)
+    | BinOp (Expr a) String (Expr a)
 
-parseDisj :: Parser (Expr Bool)
-parseDisj = do
-    e1 <- parseExpr
-    char '\\'
-    char '/'
-    Disj e1 <$> parseExpr
+-- Helpers --
+whitespace :: Parser ()
+whitespace =
+    choice
+        [ simpleWhitespace *> whitespace
+        , lineComment *> whitespace
+        , blockComment *> whitespace
+        , return ()
+        ]
+  where
+    lineComment =
+        try (string "//")
+            *> manyTill anyChar (void (char '\n') <|> eof)
+    blockComment =
+        try (string "/**")
+            *> manyTill anyChar (try $ string "*/")
+    simpleWhitespace = void $ many1 (oneOf " \t\n")
 
-parseConj :: Parser (Expr Bool)
-parseConj = do
-    e1 <- parseExpr
-    char '\\'
-    char '/'
-    Conj e1 <$> parseExpr
+-- Helpers --
 
-parseExpr :: (a ~ Bool) => Parser (Expr a)
-parseExpr = parseBool <|> parseDisj <|> parseConj
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whitespace
+
+int :: Parser Int
+int = read <$> lexeme (many1 digit)
+
+iden :: Parser String
+iden = lexeme ((:) <$> firstChar <*> many nonFirstChar)
+  where
+    firstChar :: Parser Char
+    firstChar = letter <|> char '_'
+
+    nonFirstChar :: Parser Char
+    nonFirstChar = digit <|> firstChar
+
+num :: Parser (Expr a)
+num = LitInt <$> int
+
+boolT :: Parser (Expr a)
+boolT = string "true" >> pure (LitBool True)
+
+boolF :: Parser (Expr a)
+boolF = string "false" >> pure (LitBool False)
+
+var :: Parser (Expr a)
+var = LitIden <$> iden
+
+term :: Parser (Expr a)
+term = num <|> boolT <|> boolF <|> var
